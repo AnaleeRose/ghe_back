423b1aaaba8c9895856dc0477f45589aeff50f11
Savannah S <48890044+AnaleeRose@users.noreply.github.com>
2022-09-29 22:10:51 -0500

    updated circuit query

--- controllers/circuit.js
+++ controllers/circuit.js
@@ -2,7 +2,7 @@ const { db } = require("../models");
 
 module.exports.displayMatchups = function(req, res) {
     const id = parseInt(req.params.id);
-    db.any("SELECT * FROM matches WHERE circuit_id = $1", [id])
+    db.any("select  matche.id match_id,  matche.name match_name,  matche.match_number match_number,  matche.round round,  matche.next_match_id next_match_id,  team1.id team_1_id,  team1.name team_1_name,  team2.id team_2_id,  team2.name team_2_name,  circuit.total_rounds FROM matches as matche LEFT JOIN circuits as circuit ON matche.circuit_id = circuit.id LEFT JOIN teams as team1 ON team1.id = matche.team_1_id LEFT JOIN teams as team2 ON team2.id = matche.team_2_id WHERE matche.circuit_id = $1;", [id])
         .then(function(data) {
             if (data !== null && data.length > 0) {
                 res.status(200).json({ data })
@@ -11,7 +11,8 @@ module.exports.displayMatchups = function(req, res) {
             }
         })
         .catch(function(e) {
-            console.log("GET ALL USERS FAILED");
+            console.log("GET ALL MATCHES FAILED");
             console.log(e);
         });
 }
+
