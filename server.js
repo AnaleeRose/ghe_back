801c2ad4d46f247fa9f0e402e12d7774c1729172
Savannah S <48890044+AnaleeRose@users.noreply.github.com>
2022-11-07 01:05:24 -0600

    updated server port to 8000

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const app = require('./app')
-const port = process.env.PORT || 4000
+const port = process.env.PORT || 8000
 
 const env = process.env.NODE_ENV || 'development';
 const config = require(__dirname + '/config/config.js')[env];
