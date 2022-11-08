6c3bb55fdb654d64c373e2430411e98bb11c90b2
Savannah S <48890044+AnaleeRose@users.noreply.github.com>
2022-11-07 19:51:30 -0600

    log ci circuit page

--- controllers/circuit.js
+++ controllers/circuit.js
@@ -1,6 +1,7 @@
 const { db } = require("../models");
 
 module.exports.displayMatchups = function(req, res) {
+    console.log("ENV: " + process.env.NODE_ENV);
     const id = parseInt(req.params.id);
     db.any("select  matche.id match_id,  matche.name match_name,  matche.match_number match_number,  matche.round round,  matche.next_match_id next_match_id,  team1.id team_1_id,  team1.name team_1_name,  team2.id team_2_id,  team2.name team_2_name,  circuit.total_rounds FROM matches as matche LEFT JOIN circuits as circuit ON matche.circuit_id = circuit.id LEFT JOIN teams as team1 ON team1.id = matche.team_1_id LEFT JOIN teams as team2 ON team2.id = matche.team_2_id WHERE matche.circuit_id = $1;", [id])
         .then(function(data) {
