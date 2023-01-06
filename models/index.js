af9e544da41fea283fd884a64f1b0aee13c577c9
Savannah S <48890044+AnaleeRose@users.noreply.github.com>
2023-01-06 03:58:08 -0600

    added common functions to base model

--- models/index.js
+++ models/index.js
@@ -5,4 +5,16 @@ const pgp = require("pg-promise")(initoptions);
 const connection_details = "postgres://ghe_admin:sqlshar3d@" + process.env.DB_URL + ":5432/ghe_dev";
 const db = pgp(connection_details);
 
-module.exports = { db };
+var Common = (function() {
+    var isInt = function(value) {
+        return !isNaN(value) && 
+            parseInt(Number(value)) == value && 
+            !isNaN(parseInt(value, 10));
+    };
+
+    return {
+        isInt: isInt,
+    }
+})();
+
+module.exports = { db, Common };
