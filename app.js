a5111cc9bda18286a11e1b67525a71b7944385e9
Savannah S <48890044+AnaleeRose@users.noreply.github.com>
2023-01-06 03:55:32 -0600

    added bodyparser, added routes, status now 200

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const cors = require('cors')
 // const session = require('express-session')
 const Keygrip = require('keygrip')
 const cookieSession = require('cookie-session')
+const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
 const routes = require('./routes')
 
@@ -18,7 +19,7 @@ const corsOptions = {
     // from which URLs do we want to accept requests
     origin: process.env.FRONT_URL,
     credentials: true, 
-    optionsSuccessStatus: 204
+    optionsSuccessStatus: 200
 }
 // const passport = require('./passport')
 
@@ -28,14 +29,16 @@ const app = express()
 app.use(session)
 app.use(express.json())
 app.use(cors(corsOptions));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true })); 
 
-app.get('/',(req,res)=>
-    {
-    if(req.session.isPopulated)
-        res.status(200).json(req.session)
-    else
-        res.redirect('/auth')
-})
+// app.get('/',(req,res)=>
+//     {
+//     if(req.session.isPopulated)
+//         res.status(200).json(req.session)
+//     else
+//         res.redirect('/auth')
+// })
 
 app.get('/views', function (req, res, next) {
     // Update views
@@ -46,11 +49,13 @@ app.get('/views', function (req, res, next) {
 })
 
 app.use('/auth', routes.auth);
+app.use('/circuit', routes.circuit);
+app.use('/team', routes.team);
+app.use('/teams', routes.team);
 app.use('/user', routes.user);
 app.use('/users', routes.user);
-app.use('/circuit', routes.circuit);
 app.get('*',(req,res) => {
-    res.status(204).json({status: false,message:"default"})
+    res.status(200).json({status: false,message:"default"})
 })
 
 module.exports = app;
